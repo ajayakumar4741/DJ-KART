e4e01c537707f9f132446bd243032ee5e74b7f74
Ajaya Kumar <ajayakumar4741@gmail.com>
2025-09-19 14:19:58 +0530

    added basedir

--- mydj/settings.py
+++ mydj/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'mydj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
